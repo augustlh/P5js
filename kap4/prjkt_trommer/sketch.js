a364481c4d03bce4aae452a3dd32120a24137add
August Hedman <augustlh@icloud.com>
2022-09-16 11:10:02 +0200

    Tegner cirklerne og spiller noder ud fra en vilkårlig knap

--- kap4/prjkt_trommer/sketch.js
+++ kap4/prjkt_trommer/sketch.js
@@ -1,14 +1,27 @@
 let d = 100;
 let x = 0+d/2;
+let letr;
+let song;
+var binds = ['Q','W','E','R'];
+
+let 
+
 function setup() {
   createCanvas(400, 400);
   background(120,23,23);
+
 }
 
 function draw() {
   for(i = 0; i <= width/2;i++){
     circle(x,height/2,d);
+    letr = '' + binds[i];
+    text(letr,x-5,height/2+5);
     x += d;
   }
-  console.log('lol')
+}
+
+function keyPressed(){
+    
+    console.log('assets/node' + event.key + '.mp3');
 }
