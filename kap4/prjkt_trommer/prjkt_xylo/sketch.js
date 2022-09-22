835b858c157d74cff54b0d9a8298f52aa8217012
August Hedman <augustlh@icloud.com>
2022-09-22 16:53:55 +0200

    comments

--- kap4/prjkt_trommer/prjkt_xylo/sketch.js
+++ kap4/prjkt_trommer/prjkt_xylo/sketch.js
@@ -1,33 +1,18 @@
+//deklaration af globale variabler
 let d;
-let h = 100;
+let h = 100; //global variabel h tildeles værdien 100
 let x, y, z, j, k, l;
 let ca, cb, cc;
-var binds = [
-  "q",
-  "w",
-  "e",
-  "r",
-  "t",
-  "y",
-  "u",
-  "i",
-  "o",
-  "1",
-  "2",
-  "3",
-  "4",
-  "5",
-  "6",
-  "7",
-  "8",
-];
+let speed = 4; //global variabel speed tildeles værdien 100
 
+//læser lydfilerne ind
 function preload() {
-  cas = loadSound("assets/1.wav");
+  cas = loadSound("assets/1.mp3");
   cbs = loadSound("assets/2.mp3");
   ccs = loadSound("assets/3.mp3");
 }
 
+//setup funktionen, skaber canvas og tildeler værdier til globale variabler - foregår her, fordi width ikke er ærklæret før setup
 function setup() {
   createCanvas(600, 600);
   background(156);
@@ -42,46 +27,56 @@ function setup() {
   l = -d;
 }
 
+//kører hver frame
 function draw() {
   background(23);
-  if (ca === true) {
+  animate();
+}
+
+//laver en funktion der kan bruges til at tegne en rektangel, som kan bevæge sig
+function animate(){
+  if (ca === true) { //hvis ca er true, så tegnes rektanglen, og y koordinaten ændres med speed
     rect(x, y, d, d);
-    y += 4;
-    if (y + (d / 2) * 1.8 >= height) {
-      ca = false;
-      y = -d;
+    y += speed;
+    if (y + (d / 2) * 1.8 >= height) { //dette fungerer som et border-check. Hvis rektanglen når bunden af canvas, så stoppes den
+      ca = false; //sætter ca til false, så rektanglen ikke tegnes igen
+      y = -d; //sætter y koordinaten tilbage til start
+      cas.stop() //stopper lyden
     }
   }
   if (cb === true) {
     rect(z, j, d, d);
-    j -= 5;
+    j -= speed;
     if (j + (d / 2) * 0.4 <= 0) {
       cb = false;
       j = height;
+      cbs.stop();
     }
   }
   if (cc === true) {
     rect(k, l, d, d);
-    l += 5;
+    l += speed;
     if (l + (d / 2) * 1.8 >= height) {
       cc = false;
       l = -d;
+      ccs.stop()
     }
   }
 }
 
+//funktion der håndterer keypresses
 function keyPressed() {
-  if (key === "q" && ca != true) {
+  if (key === "q" && ca != true) { //hvis q er trykket og ca ikke er true, så sættes ca til true, og lyden afspilles
     fill(255);
     ca = true;
-    cas.play();
+    cas.loop();
   }
-  if (key === "w" && cb != true) {
+  if (key === "w" && cb != true) { //hvis w er trykket og cb ikke er true, så sættes cb til true, og lyden afspilles
     fill(255);
     cb = true;
     cbs.play();
   }
-  if (key === "e" && cc != true) {
+  if (key === "e" && cc != true) { //hvis e er trykket og cc ikke er true, så sættes cc til true, og lyden afspilles
     fill(255);
     cc = true;
     ccs.play();
