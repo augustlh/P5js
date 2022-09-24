66f5a3150925cc5f7ea198eeaf1c92f7571eafbc
August Hedman <augustlh@icloud.com>
2022-09-24 12:16:26 +0200

    Forklarende kommentarer

--- kap4/prjkt_trommer/trommer/sketch.js
+++ kap4/prjkt_trommer/trommer/sketch.js
@@ -1,11 +1,18 @@
+/*
+August Leander Hedman
+S2n
+Programmering B, P5.js
+*/
+//de globale variabler erklæres
 let d = 100;
 let x = 0 + d / 2;
 let letr;
 let song;
 let key;
-//deklaration af arrayet binds, som angiver, hvilke knapper, der afspiller lyde
+//arrayet binds erklæres og tildeles værdier, som repræsenterer tilladte keypresses
 var binds = ['q', 'w','e', 'r'];
 
+//kører en gang, når programmet starter
 function setup() {
   createCanvas(400, 400);
   background(120, 23, 23);
@@ -25,12 +32,11 @@ function setup() {
 
 function draw() {}
 
-//boolean function
+//funktion der håndterer keypresses
 function keyPressed() {
   //tjekker om knappen der er trykket på er i binds arrayet
   if (binds.includes("" + event.key)) {
-    //console.log('assets/node' + event.key + '.mp3');
-    //hvis den er i arrayet, definerer den song variablen som en ny p5.sound, afhængig af hvilken knap der er trykket på
+    //hvis den er i arrayet, tildeler den song variablen en ny p5.sound, afhængig af hvilken knap der er trykket på
     song = new Audio("assets/node" + event.key + ".mp3");
     //spiller lyden
     song.play();
