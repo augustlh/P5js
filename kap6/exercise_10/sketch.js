cf91dd21960d048c99d7028c132c024176dc59b4
August Hedman <augustlh@icloud.com>
2022-11-30 11:03:26 +0100

    Working parabola

--- kap6/exercise_10/sketch.js
+++ kap6/exercise_10/sketch.js
@@ -2,15 +2,18 @@ function setup() {
   createCanvas(400, 400);
   background(220);
   andenGrads(-4, 8, 1);
-  strokeWeight(30);
-}
+  strokeWeight(2);
+  drawParabola(0.1, 1, 1);
 
-function draw() {
+  push();
+  stroke(255, 23, 23);
+  strokeWeight(1);
   line(width / 2, 0, width / 2, height);
   line(0, height / 2, width, height / 2);
-  drawParabola(6, 14, 9);
+  pop();
 }
 
+function draw() {}
 function andenGrads(a, b, c) {
   x1 = -b + (Math.sqrt(Math.pow(b, 2) - 4 * a * c) / 2) * a;
   x2 = -b - (Math.sqrt(Math.pow(b, 2) - 4 * a * c) / 2) * a;
@@ -19,10 +22,15 @@ function andenGrads(a, b, c) {
 }
 
 function drawParabola(a, b, c) {
-  for (let x = -width / 2; x < width / 2; x++) {
+  let k = [];
+  for (let x = -width; x < width; x += 0.01) {
     let y = a * Math.pow(x, 2) + b * x + c;
-    point(x + width / 2, height / 2 - y);
+    point(x + width / 2, -y + height / 2);
+    k.push(createVector(x + width / 2, -y + height / 2));
   }
+  beginShape();
+  for (let v in k) {
+    vertex(v.x, v.y);
+  }
+  endShape();
 }
-
-//hej noah
